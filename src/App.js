d017a1bb3f3fdeb0ddb58de573dc23dbcdce5abc
yadavravi043 <ravi18016925@gmail.com>
2022-11-21 15:27:32 +0530

    folder created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
 const App = () => {
+  const initialValue=0;
+  const increament=()=>{
+   //dispatch
+  }
+  const decreament=()=>{
+
+  }
   return (
     <>
-    <h1>redux</h1>
+      <h1>{initialValue}</h1>
+      <button onClick={increament} >Increament</button>
+      <button onClick={decreament}>Decreament</button>
     </>
   )
 }
